f2d1d63b4a377d18637cf89ee8680c7a1d17e4dd
dandrits <dandrits@users.noreply.github.com>
2017-05-12 22:45:13 +0300

    Create blink.py

---
+++ blink.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(True)
+GPIO.setup(18,GPIO.OUT)
+print "LED on"
+GPIO.output(18,GPIO.HIGH)
+time.sleep(1)
+print "LED off"
+GPIO.output(18,GPIO.LOW)
